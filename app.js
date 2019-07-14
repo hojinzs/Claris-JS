344e63cf0b599a69dbe60df5fc18525ce46d049d
KimInWook <narapass@naver.comn>
2019-07-15 05:01:33 +0900

    1. 채팅참여버튼 추가
    2. 닉네임입력란 추가
    
    todo : 접속자수 보여주는 기능 추가

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+//todo : 접속자정보 저장
+var userlist = [];
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/view/index.html');
