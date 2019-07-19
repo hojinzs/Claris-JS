aa0dee34fd1b0c6fef88060207aa653c390783fc
KimInWook <narapass@naver.comn>
2019-07-19 22:36:52 +0900

    사용자정보 저장
    사용자정보 저장 로직 추가

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var app = express();
 
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-//todo : 접속자정보 저장
 
-var userlist = [];
+//connected user info
+let userListArray = new Array(),
+    userMessage = new Array(),
+    userSocketId = null;
 
 app.use('/dist',express.static('dist'));
 app.use('/public',express.static('public'));
@@ -18,6 +20,17 @@ app.get('/test', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  //response socketid
+  userSocketId = socket.conn.id
+  io.emit('userSocketId', userSocketId);
+  //get user info(JSON)
+  socket.on('setUserInfo', function (data) {
+    //set user info to userListArray
+    userListArray.push(data);
+  });
+
+  console.log('current user count : ' + userListArray.length);
+
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
